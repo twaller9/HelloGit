00a129b7a41f28d22790eddfeee05fb201c2dc31
Dawson <Dawson@LAPTOP-4Q7UMP1O.unl.edu>
2022-08-28 18:10:20 -0500

    added my name and major

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -2,10 +2,13 @@ package hello;
 public class hello {
 
   
-   public static void main(String []args) {
+   public static void main(String[] args) {
 	   
       System.out.println("Thomas Waller");
       System.out.println("Computer Science");
+      
+      System.out.println("Dawson McGahan");
+      System.out.println("Computer Engineering");
       
    }
 }
